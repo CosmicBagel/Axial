cacd3b5f1e8645029f84ff7c129d61e1b0322905
Samuel Whitley <bling.nuke@gmail.com>
2014-08-06 03:03:19 -0600

    Started a bit of work on user accounts

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -14,7 +14,13 @@ module Treefort {
         .when("/", {
                 templateUrl: "Views/Frontpage.html",
                 controller: "FrontpageController"
-            })
+        })
+        .when("/account/create", {
+            templateUrl: "Views/CreateAccount.html"
+        })
+        .when("/account", {
+            templateUrl: "Views/Account.html"
+        })
         .otherwise({
             templateUrl: "Views/404.html"
         });
